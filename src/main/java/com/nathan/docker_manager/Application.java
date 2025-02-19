60fca6bac63b349092889549c02f83d0768570d4
nathan00pdl <nathanpaiva00@gmail.com>
2025-02-19 07:51:28 -0300

    changing the name of the application class

--- src/main/java/com/nathan/docker_manager/Application.java
+++ src/main/java/com/nathan/docker_manager/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerManagerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DockerManagerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
