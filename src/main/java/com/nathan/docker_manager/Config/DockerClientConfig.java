c3a5109b6fbc0dbf2101dcc41e7806bbfffbacbf
nathan00pdl <nathanpaiva00@gmail.com>
2025-02-20 09:05:17 -0300

    add comment to class of service

--- src/main/java/com/nathan/docker_manager/Config/DockerClientConfig.java
+++ src/main/java/com/nathan/docker_manager/Config/DockerClientConfig.java
@@ -9,6 +9,17 @@ import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 
+/*
+ * Class Responsabilities:
+ * 
+ * Definition of a BEAN method, managed by Spring, witch can be injected into other classes.
+ * 
+ * This method will return an object containing:
+ * -> Configuration of docker client with defined host and TLS authentication disabled (host connection will be via unix usocket).
+ * -> HTTP client to make requests between the application and de docker daemon.
+ * -> Final instance of the docker client with all the necessary configurations to comunicate with docker.  
+ */
+
 @Configuration
 public class DockerClientConfig {
     @Value("${docker.socket.path}")
